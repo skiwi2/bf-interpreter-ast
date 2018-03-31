8aa85785bf7a48e6f86a8e0c021493b7b377f9e4
skiwi <frank_skiwi@hotmail.com>
2018-03-31 22:21:55 +0200

    Implemented parsing of the input string into BFProgram.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -4,13 +4,46 @@ module Interpreter
 
 import Tape
 
-data BFInstruction = Right | Left | Increment | Decrement | Output | Input | Loop [BFInstruction] | Stop deriving (Eq, Show)
+data BFInstruction = MemoryRight | MemoryLeft | Increment | Decrement | Output | Input | Loop [BFInstruction] deriving (Eq, Show)
 type BFProgram = [BFInstruction]
 
 newtype BFMemoryCell = BFMemoryCell Int deriving (Eq, Show)
 type BFMemory = Tape BFMemoryCell
 
-interpret :: String -> IO (BFProgram, BFMemory)
-interpret program = return ([Stop], Tape [] (BFMemoryCell 0) [])
+makeProgram :: String -> BFProgram
+makeProgram = makeProgram'
 
+makeProgram' :: String -> BFProgram
+makeProgram' "" = []
+makeProgram' (x:xs) = case x of
+    '>' -> continue MemoryRight
+    '<' -> continue MemoryLeft
+    '+' -> continue Increment
+    '-' -> continue Decrement
+    '.' -> continue Output
+    ',' -> continue Input
+    '[' -> do
+        let (loop, rest) = breakLoopEnd xs
+        Loop (makeProgram loop):makeProgram' (drop 1 rest)
+    ']' -> []
+    _   -> makeProgram' xs
+    where
+        continue instr = instr:makeProgram' xs
+
+breakLoopEnd :: String -> (String, String)
+breakLoopEnd = breakLoopEnd' 0
+
+breakLoopEnd' :: Int -> String -> (String, String)
+breakLoopEnd' _ "" = error "No matching ] found"
+breakLoopEnd' 0 xs@(']':_) = ([], xs)
+breakLoopEnd' nesting (x:xs') = case x of
+    ']' -> (x:ys, zs) where (ys, zs) = next (subtract 1)
+    '[' -> (x:ys, zs) where (ys, zs) = next (+1)
+    _   -> (x:ys, zs) where (ys, zs) = next id
+    where
+        next func = breakLoopEnd' (func nesting) xs'
+
+
+interpret :: String -> IO (BFProgram, BFMemory)
+interpret program = return (makeProgram program, Tape [] (BFMemoryCell 0) [])
 
