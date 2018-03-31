01f1f24bebf299486dac5891be00ed68cc92b058
skiwi <frank_skiwi@hotmail.com>
2018-03-31 22:27:04 +0200

    Now splitting on the end of the loop rather than breaking.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -23,25 +23,25 @@ makeProgram' (x:xs) = case x of
     '.' -> continue Output
     ',' -> continue Input
     '[' -> do
-        let (loop, rest) = breakLoopEnd xs
-        Loop (makeProgram loop):makeProgram' (drop 1 rest)
+        let (loop, rest) = splitOnLoopEnd xs
+        Loop (makeProgram loop):makeProgram' rest
     ']' -> []
     _   -> makeProgram' xs
     where
         continue instr = instr:makeProgram' xs
 
-breakLoopEnd :: String -> (String, String)
-breakLoopEnd = breakLoopEnd' 0
+splitOnLoopEnd :: String -> (String, String)
+splitOnLoopEnd = splitOnLoopEnd' 0
 
-breakLoopEnd' :: Int -> String -> (String, String)
-breakLoopEnd' _ "" = error "No matching ] found"
-breakLoopEnd' 0 xs@(']':_) = ([], xs)
-breakLoopEnd' nesting (x:xs') = case x of
+splitOnLoopEnd' :: Int -> String -> (String, String)
+splitOnLoopEnd' _ "" = error "No matching ] found"
+splitOnLoopEnd' 0 (']':xs') = ([], xs')
+splitOnLoopEnd' nesting (x:xs') = case x of
     ']' -> (x:ys, zs) where (ys, zs) = next (subtract 1)
     '[' -> (x:ys, zs) where (ys, zs) = next (+1)
     _   -> (x:ys, zs) where (ys, zs) = next id
     where
-        next func = breakLoopEnd' (func nesting) xs'
+        next func = splitOnLoopEnd' (func nesting) xs'
 
 
 interpret :: String -> IO (BFProgram, BFMemory)
